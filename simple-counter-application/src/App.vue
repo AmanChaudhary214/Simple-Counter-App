c0db8a83f0933a98f123bd9678f41d971c228475
Aman Chaudhary <aman.chaudhary214@gmail.com>
2023-08-22 21:55:22 +0530

    code added

---
+++ simple-counter-application/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <p>Count: {{ count }}</p>
+    <button v-on:click="increment">Increase Count</button>
+    <button v-on:click="decrement">Decrease Count</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment: function() {
+      this.count++;   // Increment the count
+    },
+    decrement: function() {
+      this.count--;   // Decrement the count
+    }
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+p {
+  font-size: 40px;
+}
+
+button {
+  font-size: 20px;
+  padding: 10px 20px 10px;;
+  border-radius: 5px;
+  margin-left: 20px;
+}
+</style>
